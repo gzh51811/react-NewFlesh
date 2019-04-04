9623e3506c4b8b490e1ce892e1b6de52147af96e
4343545 <35435345@qq.com>
2019-04-04 15:36:59 +0800

    sss

--- src/component/home_main/Home_main_goodslist.js
+++ src/component/home_main/Home_main_goodslist.js
@@ -3,20 +3,52 @@ import React from 'react'
 
 function Home_main_goodslist() {
     return (
+        <div>
+            <div className="banner-img" style={{ width: '100%' }}>
+                <img src="https://j-image.missfresh.cn/mis_img_20190226141252067.jpg?mryxw=750&amp;mryxh=142"
+                    className="image-view divider-banner" style={{ width: '100%' }} />
+            </div>
 
-        <div className="checkitemli">
-
-            <div >
-                <img className="imgs" src="https://fms-image.missfresh.cn/02e6b07f9ad64369a25a71179eb49627.jpg?iopcmd=thumbnail&amp;type=4&amp;width=200" />
-                <div className="conter" >
-                    <p className="p1"> 鸡蛋就都几点男法师打发的</p>
-                    <p className="p2">鸡蛋就都几点男法师打的</p>
-                    <div className="price">
-                        <span style={{ color: 'red', }}>￥99.00</span>
+            <div className="checkitemli">
+                <div >
+                    <img className="imgs" src="https://fms-image.missfresh.cn/02e6b07f9ad64369a25a71179eb49627.jpg?iopcmd=thumbnail&amp;type=4&amp;width=200" />
+                    <div className="conter" >
+                        <p className="p1"> 鸡蛋就都几点男法师打发的</p>
+                        <p className="p2">鸡蛋就都几点男法师打的</p>
+                        <div className="price">
+                            <span style={{ color: 'red', }}>￥99.00</span>
+                        </div>
                     </div>
                 </div>
+                <img className="cartbtnimg" src={[require('../../static/image/cart1.png')]} />
             </div>
-            <img className="cartbtnimg" src={[require('../../static/image/cart1.png')]} />
+            <div className="checkitemli">
+                <div >
+                    <img className="imgs" src="https://fms-image.missfresh.cn/02e6b07f9ad64369a25a71179eb49627.jpg?iopcmd=thumbnail&amp;type=4&amp;width=200" />
+                    <div className="conter" >
+                        <p className="p1"> 鸡蛋就都几点男法师打发的</p>
+                        <p className="p2">鸡蛋就都几点男法师打的</p>
+                        <div className="price">
+                            <span style={{ color: 'red', }}>￥99.00</span>
+                        </div>
+                    </div>
+                </div>
+                <img className="cartbtnimg" src={[require('../../static/image/cart1.png')]} />
+            </div>
+            <div className="checkitemli">
+                <div >
+                    <img className="imgs" src="https://fms-image.missfresh.cn/02e6b07f9ad64369a25a71179eb49627.jpg?iopcmd=thumbnail&amp;type=4&amp;width=200" />
+                    <div className="conter" >
+                        <p className="p1"> 鸡蛋就都几点男法师打发的</p>
+                        <p className="p2">鸡蛋就都几点男法师打的</p>
+                        <div className="price">
+                            <span style={{ color: 'red', }}>￥99.00</span>
+                        </div>
+                    </div>
+                </div>
+                <img className="cartbtnimg" src={[require('../../static/image/cart1.png')]} />
+            </div>
+
         </div>
 
     )
--- src/pages/Home_main.js
+++ src/pages/Home_main.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+// import { Carousel } from 'antd';
+
 import '../css/home_main.scss';
 
 import Home_main_goodslist from '../component/home_main/Home_main_goodslist.js'
@@ -15,10 +17,73 @@ class Home_main extends Component {
                     </a>
                 </div>
 
-                <nav>
+                {/* {小导航} */}
+                <section className="minnav">
+                    <ul>
+                        <li>画册</li>
+                        <li>画册</li>
+                        <li>画册</li>
+                        <li>画册</li>
+                    </ul>
+                </section>
 
+
+                {/* 保障 */}
+                <section className="recommend-title">
+                    <div className="recommend-text">
+                        <img src="https://j-image.missfresh.cn/img_20170627185311186.png" />
+                        <em>优鲜严选</em>
+                    </div>
+                    <div className="recommend-text">
+                        <img src="https://j-image.missfresh.cn/img_20170627184654084.png" />
+                        <em>安心检测</em>
+                    </div>
+                    <div className="recommend-text">
+                        <img src="https://j-image.missfresh.cn/img_20170718194948016.png" />
+                        <em>赔付保障</em>
+                    </div>
+                </section>
+
+
+                {/* 导航 */}
+                <nav className="main-nav">
+                    <div className="lantern-item">
+                        <div className="lantern-item-img">
+                            <img className='item-img' src={[require('../static/image/nav1.png')]} />
+                        </div>
+                        <p>本周新品</p>
+                    </div>
+                    <div className="lantern-item">
+                        <div className="lantern-item-img">
+                            <img className='item-img' src={[require('../static/image/nav2.png')]} />
+                        </div>
+                        <p>邀请有礼</p>
+                    </div>
+                    <div className="lantern-item">
+                        <div className="lantern-item-img">
+                            <img className='item-img' src={[require('../static/image/nav3.png')]} />
+                        </div>
+                        <p>每日签到</p>
+                    </div>
+                    <div className="lantern-item">
+                        <div className="lantern-item-img">
+                            <img className='item-img' src={[require('../static/image/nav4.png')]} />
+                        </div>
+                        <p>凑单专区</p>
+                    </div>
+                    <div className="lantern-item">
+                        <div className="lantern-item-img">
+                            <img className='item-img' src={[require('../static/image/nav5.png')]} />
+                        </div>
+                        <p>开通会员</p>
+                    </div>
                 </nav>
 
+                {/* 两张banner图 */}
+                <div className="card-wrap">
+                    <img src="https://j-image.missfresh.cn/img_20190115153812265.png" class="image-view" />
+                    <img src="https://j-image.missfresh.cn/img_20190115153756830.png" class="image-view" />
+                </div>
 
                 <Home_main_goodslist></Home_main_goodslist>
 
